a1ca95c6682b8a7ed8477937fadecbceba54ecca
Vaibhav Bhagat <vaibhavbhagat7461@gmail.com>
2025-05-22 20:41:03 +0530

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
