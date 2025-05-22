d1a4f78682c50d8da1df8a7c215882c20364650d
Vaibhav Bhagat <vaibhavbhagat7461@gmail.com>
2025-05-22 20:17:59 +0530

    Create Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Navbar() {
+  return (
+    <nav className="bg-blue-600 p-4 text-white text-center text-xl font-semibold">
+      SmartNotes
+    </nav>
+  );
+}
